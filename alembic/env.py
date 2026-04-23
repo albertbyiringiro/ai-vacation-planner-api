a8ab641232721a4cbf6b3c19abfbe1b822e214d2
Albert Byiringiro <albert.byiringiro@proton.me>
2026-04-23 10:10:44 +0200

    refactor: update alembic

--- alembic/env.py
+++ alembic/env.py
@@ -105,6 +105,7 @@ def run_migrations_online() -> None:
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
+            render_as_batch=True,
             render_item=render_item,
         )
 
